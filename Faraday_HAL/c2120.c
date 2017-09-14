6b8ed6f87cfa148ce7deca585463d764f8fed48a
Brenton Salmi <brent@faradayrf.com>
2017-09-13 23:14:48 -0700

    Adding c2120 header and source.

---
+++ Faraday_HAL/c2120.c
@@ -0,0 +1,10 @@
+/*
+ * c2120.c
+ *
+ *  Created on: Sep 13, 2017
+ *      Author: Brent
+ */
+
+
+
+
---
+++ Faraday_HAL/c2120.h
@@ -0,0 +1,13 @@
+/*
+ * c2120.h
+ *
+ *  Created on: Sep 13, 2017
+ *      Author: Brent
+ */
+
+#ifndef FARADAY_HAL_C2120_H_
+#define FARADAY_HAL_C2120_H_
+
+
+
+#endif /* FARADAY_HAL_C2120_H_ */
